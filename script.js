48369d50ddbf49950baddf3e9bde7f5eeb13ea27
Purvesh Tawade <purveshtawade@gmail.com>
2026-03-13 15:45:05 +0530

    Implemented edit, delete, completion toggle and task counters

--- script.js
+++ script.js
@@ -44,3 +44,43 @@ const deleteBtn = li.querySelector(".delete-btn");
 checkbox.addEventListener("click", function () {
   li.classList.toggle("completed", checkbox.checked);
 });
+editBtn.addEventListener("click", function () {
+  const update = prompt("Edit task:", taskSpan.textContent);
+  if (update !== null) {
+    taskSpan.textContent = update;
+    li.classList.remove("completed");
+  }
+});
+li.classList.remove("completed");
+const completedCounter = document.getElementById("completed-counter");
+const uncompletedCounter = document.getElementById("uncompleted-counter");
+function updateCounters() {
+  const completedTasks = document.querySelectorAll(".completed").length;
+  const uncompletedTasks =
+    document.querySelectorAll("li:not(.completed)").length;
+
+  completedCounter.textContent = completedTasks;
+  uncompletedCounter.textContent = uncompletedTasks;
+}
+updateCounters();
+checkbox.addEventListener("click", function () {
+  li.classList.toggle("completed", checkbox.checked);
+  //add the function below
+  updateCounters();
+});
+editBtn.addEventListener("click", function () {
+  const update = prompt("Edit task:", taskSpan.textContent);
+  if (update !== null) {
+    taskSpan.textContent = update;
+    li.classList.remove("completed");
+    //add the code below
+    checkbox.checked = false;
+    updateCounters();
+  }
+});
+deleteBtn.addEventListener("click", function () {
+  if (confirm("Are you sure you want to delete this task?")) {
+    li.remove();
+    updateCounters();
+  }
+});
