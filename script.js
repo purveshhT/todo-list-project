18d5b591d036a0d46690187284ce10970a3b6fe6
Purvesh Tawade <purveshtawade@gmail.com>
2026-03-13 15:36:26 +0530

    Added checkbox, edit and delete functionality

--- script.js
+++ script.js
@@ -17,3 +17,30 @@ li.innerHTML = `
   <span class="delete-btn">Delete</span>
 `;
 listContainer.appendChild(li);
+inputBox.value = "";
+function addTask() {
+  const task = inputBox.value.trim();
+  if (!task) {
+    alert("Please write down a task");
+    return;
+  }
+
+  li.innerHTML = `
+    <label>
+      <input type="checkbox">
+      <span>${task}</span>
+    </label>
+    <span class="edit-btn">Edit</span>
+    <span class="delete-btn">Delete</span>
+    `;
+
+  listContainer.appendChild(li);
+  inputBox.value = "";
+}
+const checkbox = li.querySelector("input");
+const editBtn = li.querySelector(".edit-btn");
+const taskSpan = li.querySelector("span");
+const deleteBtn = li.querySelector(".delete-btn");
+checkbox.addEventListener("click", function () {
+  li.classList.toggle("completed", checkbox.checked);
+});
